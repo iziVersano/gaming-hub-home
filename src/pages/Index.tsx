0ebcba2e5e1e0a6ea4c5706ae51f01ce9902d0a8
Izi <izi@consoltech.co.il>
2026-04-05 23:15:24 +0000

    fix(hero/mobile): move bg image up to 210px

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -80,7 +80,7 @@ const Index = () => {
             src="/images/bckmobil.png"
             alt=""
             aria-hidden="true"
-            style={{ position: 'absolute', top: '255px', left: 0, width: '100%', height: 'auto' }}
+            style={{ position: 'absolute', top: '210px', left: 0, width: '100%', height: 'auto' }}
           />
         </div>
 
